b2e766e15b927faca94c4fb6f761170cb06f519f
Anthony Saxon <anthony.saxon@infiniteblue.com>
2022-09-13 13:14:38 -0400

    adding RDS endpoint

--- inventory.py
+++ inventory.py
@@ -470,8 +470,9 @@ def rds_instances_list(session):
                 rds_instance_create_date = rds_instance['InstanceCreateTime']
                 rds_instance_engine = rds_instance['Engine']
                 rds_instance_type = rds_instance['DBInstanceClass']
+                rds_endpoint_address = rds_instance['Endpoint']['Address']
                 #rds_instance_size = rds_instance.
-                rds_instances.append([rds_instance_name, rds_instance_az, session.profile_name, rds_instance_engine, rds_instance_type, str(rds_instance_create_date.replace(tzinfo=None))])
+                rds_instances.append([rds_instance_name, rds_endpoint_address, rds_instance_az, session.profile_name, rds_instance_engine, rds_instance_type, str(rds_instance_create_date.replace(tzinfo=None))])
         except Exception as e:
             print("Error getting RDS instances for profile: {}".format(session.profile_name) + ": " + str(e))
             traceback.print_exc()
@@ -590,7 +591,7 @@ def main():
     # Create a list of RDS instances.
     rds_instances = [rds_instances_list(session) for session in sessions]
     rds_instances_flat = [item for sublist in rds_instances for item in sublist]
-    rds_instances_flat.insert(0,["DB Identifier", "Availability Zone", "Profile", "Engine", "Size", "Create Date"])
+    rds_instances_flat.insert(0,["DB Identifier", "DB Endpoint", "Availability Zone", "Profile", "Engine", "Size", "Create Date"])
     # Write RDS instances to spreadsheet.
     write_worksheet(workbook, "RDS Instances", rds_instances_flat)
     # Create a list of EFS filesystems.
